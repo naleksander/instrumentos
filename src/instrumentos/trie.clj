15136a93e18c8ff3a4ba00fca27e3fb8a1ed4965
naleksander <aleksander.naszko@gmail.com>
2011-12-11 11:56:45 +0100

    added fetching keys

--- src/instrumentos/trie.clj
+++ src/instrumentos/trie.clj
@@ -10,7 +10,7 @@
 			[item])))
 
 (defn key-seq[ key ]
-	(mapcat #(vector %1 (str %2)) (repeat :node) (seq key)))
+	(cons :node (interpose :node key)))
 
 (defn tassoc[ col key item ]
 	(assoc-in col (snoc (key-seq key) :val) item))
@@ -21,15 +21,18 @@
 			(snoc rst val)
 			rst)))
 
+(defn tderef-keys[ col word ]
+	(let [ rst  (mapcat (fn[ [ k v] ] (tderef-keys v (str word k)) ) (:node col)) ]
+		(if-let[ val (:val col) ]
+			(cons word rst)
+					rst)))
+	
 (defn tget[ col key ]
 	(tderef (get-in col (key-seq key))))
 
-;
-;(tderef (tget g ""))
-;
-;(tderef g)
-;
-;(tderef (tget g "ab"))
+(defn tkeys[ col key ]
+	(tderef-keys (get-in col (key-seq key)) key))
+
 
 (def g { :node { "a" { :val 2
   		               :node { "b" { :val 3
@@ -41,20 +44,39 @@
 							     				   :node { "g" { :val 55 }}}}}}}
 		}})
 
+(def g (-> {}
+	(tassoc "a" 2)
+	(tassoc "ab" 3)
+	(tassoc "abz" 4)
+	(tassoc "abzg" 5)
+	(tassoc "brokowski" 4)
+	(tassoc "b" 22)
+	(tassoc "bb" 33)
+	(tassoc "bbz" 44)
+	(tassoc "bbzg" 55)
+	(tassoc "brozowski" 5)))
 
-(def z (-> {}
+(println (tkeys g "b"))
+
+
+(println (tkeys (-> {}
 	(tassoc "" 3)
 	(tassoc "ala" 2)
 	(tassoc "alaza" 4)
-	(tassoc "a" 3)))
+	(tassoc "a" 3)) "a"  ))
 
-(tassoc {} "" 3)
-
-(println z)
-
-(println (tget z "alaz"))
-
-(snoc (snoc (snoc [ 1 2 3] 4) 6) 7)
+;(defn scaffold [iface] 
+;  (doseq [[iface methods] (->> iface .getMethods 
+;                            (map #(vector (.getName (.getDeclaringClass %)) 
+;                                    (symbol (.getName %)) 
+;                                    (count (.getParameterTypes %)))) 
+;                            (group-by first))] 
+;    (println (str "  " iface)) 
+;    (doseq [[_ name argcount] methods] 
+;      (println 
+;        (str "    " 
+;          (list name (into ['this] (take argcount (repeatedly 
+;gensym))))))))) 
 
 
 
